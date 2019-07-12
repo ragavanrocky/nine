f3e0c375668156b7e0691588fe549d75f10ebc07
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-12 16:30:04 +0530

    Create nine.py

---
+++ nine.py
@@ -0,0 +1,23 @@
+n=int(input())
+st=[]
+for i in range(0,n):
+    s=input()
+    st.append(s)
+
+i=0
+count=0
+f=True
+for i in range(0,len(st[0])):
+    if(f==False):
+        break
+    j=1
+    while(j<n and st[0][i]==st[j][i]):
+        j+=1
+    if(j==n):
+        count+=1
+    else:
+        
+        
+    
+for i in range(0,count):
+    print(st[0][i],end="")
